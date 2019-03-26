712f421f5bb98081e29727d73893e1666e3ef4b7
Mohamed Sassi <sassi360@gmail.com>
2019-03-25 23:36:55 -0400

    Setup sitemap and google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     owner: "Mohamed Sassi",
@@ -6,22 +10,33 @@ module.exports = {
       "Mohamed Sassi is a front-end developer, UI researcher and creative designer living in Toronto, ON.",
     url: "https://www.msassi.dev",
     image: "/images/icon.png",
-    twitterUsername: "@Sassi360M"
+    twitterUsername: "@Sassi360M",
+    siteUrl: "https://www.msassi.dev"
   },
   plugins: [
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: "Mohamed Sassi · Frontend Developer & Creative UI Designer",
-        short_name: "Mohamed Sassi",
+        short_name: "Sassi DEV",
         start_url: "/",
         theme_color: "#663399",
+        background_color: "#663399",
         display: `standalone`,
         icon: "src/images/icon.png",
         include_favicon: true,
         legacy: true,
         crossOrigin: `use-credentials`
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+      },
     },
     {
       resolve: "gatsby-plugin-robots-txt",
@@ -34,6 +49,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
     "gatsby-plugin-offline",
-    "gatsby-plugin-twitter"
+    "gatsby-plugin-twitter",
+    "gatsby-plugin-sitemap"
   ]
 };
