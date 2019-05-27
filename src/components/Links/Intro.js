2b95c0c5bd8412282085cbbc32a666cdb2c8e101
Mohamed Sassi <Sassi360@gmail.com>
2019-05-27 18:20:57 -0400

    Update resume, intro and style

--- src/components/Links/Intro.js
+++ src/components/Links/Intro.js
@@ -22,61 +22,41 @@ export default class Intro extends Component {
             </a>
           </div>{" "}
         </span>
-        <h3>Objective</h3>
-        <p className="textblock">
-          An opportunity to work and upgrade oneself, as well as being involved
-          in an organization that believes in gaining a competitive edge and
-          giving back to the community. I focus on using my interpersonal skills
-          to build a good user experience.I hope to develop my skills in
-          frontend development, my knowledge of the Web, and become an honest
-          asset to the business. As an individual, I 'm confident you'll find me
-          creative, funny and naturally passionate. I 'm a forward thinker, which
-          others may find inspiring when working as a team.
-        </p>
         <h3>What I do</h3>
         <p className="textblock">
-          I have 4 years of hands-on experience efficiently designing and coding
-          websites/applications using modern HTML, CSS, and JavaScript along
-          with other technology stacks that I have acquired over the years.
-          Building state-of-the-art, easy to use, user-friendly websites and
-          applications is truly a passion of mine and I am confident I would be
-          an excellent addition to your organization. I actively seek out new
-          technologies and stay up-to-date on industry trends and advancements.
-          This has allowed me to stay ahead of the curve and deliver exceptional
-          work to all of my employers, including those I've worked for on a
-          project basis.
+        As an experienced user-interface designer and front-end developer with 4+ years of commercial experience, I specialized in design/development of web applications and responsive websites with special attention to best practices. I am passionate about engaging in the professional community and following the ever-evolving web landscape, particularly with regards to emerging technologies, usability and design. I’m a keen follower of Agile and iterative design, backed by A/B & multivariate split testing.
         </p>
         <h3>Skills and technologies</h3>
         <div className="skills">
           <ul>
             <li>HTML5 & CSS3</li>
-            <li>JavaScript (ES6)</li>
+            <li>JavaScript</li>
             <li>jQuery</li>
+            <li>Git</li>
             <li>React</li>
+            <li>GatsbyJs</li>
+            <li>Apollo</li>
+            <li>GraphQL</li>
             <li>Vue</li>
-            <li>JSON</li>
+            <li>Node.js</li>
             <li>PHP</li>
-            <li>Responsive Design</li>
-            <li>Web Animation</li>
-            <li>Verison Control (GIT)</li>
-            <li>MongoDB</li>
             <li>MYSQL</li>
-            <li>Web Performance</li>
+            <li>Styled Component</li>
           </ul>
 
           <ul>
             <li>Docker/Vagrant</li>
-            <li>AWS, Digital Ocean, Netlify, Heroku</li>
+            <li>MongoDB</li>
+            <li>AWS, Digital Ocean and Netlify</li>
             <li>Sketch/Adobe XD</li>
-            <li>Wireframes</li>
+            <li>Jira/Confluence</li>
             <li>Grunt/Gulp</li>
             <li>NPM & Yarn</li>
             <li>Webpack</li>
             <li>Unit testing</li>
             <li>Search Engine Optimization (SEO)</li>
-            <li>CMS (WordPress and Drupal) </li>
+            <li>CMS (WordPress and Contentful) </li>
             <li>E-commerce (Shopify, WooCommerce)</li>
-            <li>Node.js</li>
             <li>Testing/Debugging</li>
           </ul>
         </div>
--- src/contents/experience.js
+++ src/contents/experience.js
@@ -4,8 +4,26 @@ export default [
     end_date: "Present",
     job_title: "Freelance",
     location: "Toronto, ON",
-    position: "FRONTEND DEVELOPER AND UI DESIGNER",
-    description: `• Created custom websites integrated with popular CMS platforms such as WordPress, Drupal using PHP, HTML 5, CSS 3, jQuery/JavaScript and MySQL.<br />• Built custom eCommerce Solutions on top of PHP projects using payment APIs and integrated SSL backed systems.<br />• Work with sites throughout the entire life of the project including initial development, QA testing, debugging, server administration and site launch as well as setting up Google Analytics and Webmasters tools.<br />• Produced responsive and SEO friendly user interfaces and visual designs – from concept to launch on web and mobile applications for small to medium-sized businesses. <br />• Designed visual mockups/prototypes from wireframes and delivered final design production-ready assets for development. <br />• Produced marketing assets for consumer and industry events and promotions. <br />• Technologies Used: HTML5, CSS3, BootStrap, WordPress, Drupal , jQuery, Javascript with (React.js, Angular.js and vue.js), JSON, XML, PHP, MySQL, Drupal, Node.js, Git and AJAX.<br />`
+    position: "UI FRONTEND DEVELOPER",
+    description: `
+    • Utilized latest HTML5, CSS3 and JavaScript Framework (React and Vue) technologies to build semantically correct and visually interesting responsive designs.<br/><br/>
+    • Created a medium, and small scale websites from the ground up using appropriate content management systems - WordPress, GatsbyJS (React static site generator) and Contentful CMS.<br/><br/>
+    • Built custom eCommerce Solutions (Shopify and WooCommerce) using payment APIs and integrated backend systems.<br/><br/>
+    • Work with sites throughout the entire life of the project including initial development, QA testing, debugging, server administration and site launch as well as setting up Google Analytics and Webmasters tools.<br/><br/>
+    • Produced responsive and SEO friendly user interfaces and visual designs – from concept to launch on web and mobile applications for small to medium-sized businesses.<br/><br/>
+    • UX research and testing using a variety of platforms and techniques including, but not limited to, Google Analytics, Hotjar and A/B testing.`
+  },
+  {
+    start_date: "March 2019",
+    end_date: "May 2019",
+    job_title: "Planted Tree Digital",
+    location: "Scarborough , ON",
+    position: "FRONTEND WEB DEVELOPER",
+    description: `
+    • Working in a web startup in a mostly digital environment. The focus was on inbound digital marketing, email, and web pages.<br/><br/>
+    • Designed websites, interfaces, and layouts to fit clients’ desires including developing responsive layouts to enhance the experience on mobile devices.<br/><br/>
+    • Created interactive elements with custom JavaScript and jQuery.<br/><br/>
+    • Developed responsive custom WordPress themes using HTML5, CSS3/Sass, Javascript/jQuery, PHP, MySQL, and Git.`
   },
   {
     start_date: "OCTOBER 2016",
@@ -13,11 +31,14 @@ export default [
     job_title: "TALENTNET INC",
     location: "Toronto, ON",
     position: "FRONTEND DEVELOPER",
-    description: `• Develop frontend interfaces for single page application (SPA),
-        utilizing JavaScript frameworks and libraries (Ember.JS and ReactJS.) <br />• Implement responsible web design principles to ensure that the company website renders well across multiple devices.<br />• Assist in the development of applications and features that will
-        be put on the website and in internal functions as well.<br />• Participate as part of an agile development team. <br />• Responsible for the coding of all UI interactivity. <br />• Work with sites throughout the entire life of the project including initial development, QA testing, debugging and site launch.<br />• Converting PSD/Sketch files to functional web application. <br />• Utilize frontend build tools and JavaScript tooling such as NPM,
-        Bower, Babel and Webpack.<br />• Technologies Used: HTML5, CSS3, Javascript framework
-        (Ember.js and React.js), JSON, PHP with Symfony 4 framework and
-        MongoDB.`
+    description: `
+    • Develop a contingent workforce platform interfaces and participated in site-wide front-end migration from Ember.js to ReactJs.<br/><br/>
+    • Implement web design best practices to ensure that the company website renders well across multiple devices.<br/><br/>
+    • Worked on a team of developers to coordinate and develop component library.<br/><br/>
+    • Worked in Agile methodology by briefing team in Scrum calls, suggesting story tasks and approximating efforts.<br/><br/>
+    • Work with sites throughout the entire life of the project including initial development, QA testing, debugging and deployment.<br/><br/>
+    • Work with sites throughout the entire life of the project including initial development, QA testing, debugging and site launch.<br/><br/>
+    • Used HTML5, CSS3, JavaScript, React.js, Mobx, Git, Bower, Webpack, and REST APIs to develop complex, responsive, large-scale web applications<br/><br/>
+    `
   }
 ];
